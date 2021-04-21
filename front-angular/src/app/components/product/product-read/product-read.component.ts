f8be9873e7c75ab7d3b9e1e9b328a709f93381fd
William R. Ferreira <willian.usp@hotmail.com>
2021-04-21 17:50:22 -0300

    Implementação dos métodos 'findById' e 'update' no Service

--- front-angular/src/app/components/product/product-read/product-read.component.ts
+++ front-angular/src/app/components/product/product-read/product-read.component.ts
@@ -11,7 +11,7 @@ export class ProductReadComponent implements OnInit {
 
 
   products : Product[]
-  displayedColumns = ['id', 'name', 'price']
+  displayedColumns = ['id', 'name', 'price','action']
 
   constructor(private productService: ProductService) { }
 
